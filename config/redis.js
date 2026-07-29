ce87fd7b23173895125fffc0c7b787cf11210f0c
Mahmut Tüysüz <mahmuttuysuz45@hotmail.com>
2026-07-29 07:30:54 +0300

    admin panel revise

--- config/redis.js
+++ config/redis.js
@@ -1,18 +1,28 @@
 const { createClient } = require('redis');
+const dotenv = require('dotenv');
+const dotenvExpand = require('dotenv-expand');
+dotenvExpand.expand(dotenv.config());
 
-const redisClient = createClient({
-  url: process.env.REDIS_URL || 'redis://127.0.0.1:6379',
-  RESP: 2
-});
+let clientOptions = {
+    url: process.env.REDIS_URL || 'redis://127.0.0.1:6379'
+};
+if (process.env.APP_ENV === 'dev') clientOptions.RESP = 2;
+
+const redisClient = createClient(clientOptions);
 
 redisClient.on('error', (err) => {
-  console.error('❌ Redis Bağlantı Hatası:', err);
+    console.error('❌ Redis Bağlantı Hatası:', err);
 });
 
 redisClient.on('connect', () => {
-  console.log('🔴 Redis oturum sunucusuna başarıyla bağlanıldı.');
+    console.log('🔴 Redis oturum sunucusuna başarıyla bağlanıldı.');
 });
 
-redisClient.connect().catch(console.error);
+// Asenkron Bağlantı Başlatıcı
+(async () => {
+    if (!redisClient.isOpen) {
+        await redisClient.connect();
+    }
+})();
 
 module.exports = redisClient;
